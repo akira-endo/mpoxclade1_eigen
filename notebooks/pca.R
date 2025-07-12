b516fc2d9f3890a1c1871c9014d9421f5c67f702
Akira Endo <aendo@nus.edu.sg>
2025-07-12 16:07:50 +0800

    update main and pca

---
+++ notebooks/pca.R
@@ -0,0 +1,17 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .R
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.6
+#   kernelspec:
+#     display_name: R
+#     language: R
+#     name: ir
+# ---
+
+source("../src/pca_indicators.R",chdir=T)
+
+
